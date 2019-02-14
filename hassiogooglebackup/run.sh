d6914e19150da5c64fe5c3da61fb48fc67a71944
Scott McCauley <scottmccauley64@gmail.com>
2019-02-14 14:19:13 -0500

    added export of GB_DEBUG

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -5,6 +5,7 @@ CONFIG_PATH=/data/options.json
 GB_DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
 
 echo "GB_DEBUG = $GB_DEBUG"
+export GB_DEBUG
 
 cd googlebackup
 python manage.py runserver 0.0.0.0:8000
