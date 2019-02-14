d861709b108dae1cb2c27c30ff7bf71d1e050dca
Scott McCauley <scottmccauley64@gmail.com>
2019-02-14 11:34:14 -0500

    Minor fix in run.sh

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -7,7 +7,7 @@ GB_DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
 echo "GB_DEBUG = $GB_DEBUG"
 
 cd googlebackup
-if ["$GB_DEBUG" == "true"]; then
+if ["$GB_DEBUG" = "true"]; then
     echo "Starting googlebackup add-on in debug mode"
     python manage.py runserver --settings=debug_settings 0.0.0.0:8000
 else
