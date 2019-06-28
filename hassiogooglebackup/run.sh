b5bb65eb511f30fcf54b315c7a064c26b4ae8b7a
Scott McCauley <scottmccauley64@gmail.com>
2019-06-28 13:08:58 -0400

    Output Version to console

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 
-CONFIG_PATH=/data/options.json
-GB_DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
+OPTIONS_PATH=/data/options.json
+CONFIG_PATH=/data/config.json
+
+GB_DEBUG="$(jq --raw-output '.debug' $OPTIONS_PATH)"
+GB_VERSION="$(jq --raw-output '.version' $CONFIG_PATH)"
 
 echo "GB_DEBUG = $GB_DEBUG"
+echo "GB_VERSION = $GB_VERSION"
+
 export GB_DEBUG
+export GB_VERSION
 
 cd googlebackup
 python manage.py runserver --noreload 0.0.0.0:8000
