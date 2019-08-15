b2c34b3190dd02ac504053e220afe6dcdc5bf8c5
Scott McCauley <scottmccauley64@gmail.com>
2019-08-15 14:21:29 -0400

    Made ingress ip variable

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -6,11 +6,14 @@ OPTIONS_PATH=/data/options.json
 GB_DEBUG="$(jq --raw-output '.debug' $OPTIONS_PATH)"
 GB_VERSION="1.7.0"
 
+INGRESS_IP="$(curl -H 'X-HASSIO-KEY: ${HASSIO_TOKEN}' 'http://hassio/addons/self/info' | jq -r '.ip_address')"
+
 echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
+echo "INGRESS_IP = $INGRESS_IP"
 
 export GB_DEBUG
 export GB_VERSION
 
 cd googlebackup
-python manage.py runserver --noreload 172.30.32.2:8099
+python manage.py runserver --noreload $INGRESS_IP:8099
