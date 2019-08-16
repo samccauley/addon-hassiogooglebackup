485e565378f6db83820ef3a1613a211c3293bad0
Scott McCauley <scottmccauley64@gmail.com>
2019-08-16 15:22:27 -0400

    Debugging run.sh

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -7,9 +7,12 @@ GB_DEBUG="$(jq --raw-output '.debug' $OPTIONS_PATH)"
 GB_VERSION="1.7.0"
 
 echo "HASSIO_TOKEN = $HASSIO_TOKEN"
-curl -v -H 'X-HASSIO-KEY: ${HASSIO_TOKEN}' 'http://hassio/addons/self/info'
+auth_header="X-HASSIO-KEY: ${HASSIO_TOKEN}"
+echo "auth_header = $auth_header"
 
-INGRESS_IP="$(curl -s -H 'X-HASSIO-KEY: ${HASSIO_TOKEN}' 'http://hassio/addons/self/info' | jq -r '.ip_address')"
+curl -v -H '${auth_header}' 'http://hassio/addons/self/info'
+
+INGRESS_IP="$(curl -s -H '${auth_header}' 'http://hassio/addons/self/info' | jq -r '.ip_address')"
 
 echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
@@ -17,7 +20,7 @@ echo "INGRESS_IP = $INGRESS_IP"
 
 if $GB_DEBUG; then
     echo "Here is the output from /addons/self/info"
-    curl -s -H 'X-HASSIO-KEY: ${HASSIO_TOKEN}' 'http://hassio/addons/self/info'
+    curl -s -H '${auth_header}' 'http://hassio/addons/self/info'
     echo "End of output from /addons/self/info"
 fi
 
