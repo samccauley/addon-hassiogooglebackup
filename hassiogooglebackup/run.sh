71a9871cd39a19e70780084245756bf9877147d6
Scott McCauley <scottmccauley64@gmail.com>
2019-08-16 09:37:41 -0400

    Corrected errors in run.sh

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -12,7 +12,7 @@ echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
 echo "INGRESS_IP = $INGRESS_IP"
 
-if [$GB_DEBUG == "true"]; then
+if [$GB_DEBUG]; then
     echo "Here is the output from /addons/self/info"
     curl -s -H 'X-HASSIO-KEY: ${HASSIO_TOKEN}' 'http://hassio/addons/self/info'
     echo "End of output from /addons/self/info"
