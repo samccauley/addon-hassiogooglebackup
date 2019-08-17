bbcb7549acffb0116a0c23a7f472025ec57d4c7f
Scott McCauley <scottmccauley64@gmail.com>
2019-08-17 14:04:15 -0400

    backed off to default hassio role and fixed entry url

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -9,25 +9,23 @@ GB_VERSION="1.7.0"
 auth_header="X-HASSIO-KEY: ${HASSIO_TOKEN}"
 
 json_info=$(curl -s -H "${auth_header}" 'http://hassio/addons/self/info')
-echo "Here is the json_info"
-echo $json_info
-echo -e "\nEnd of json_info"
 
-#INGRESS_IP="$(curl -s -H \"${auth_header}\" 'http://hassio/addons/self/info' | jq -r '.data.ip_address')"
 INGRESS_IP=$(echo ${json_info} | jq -r '.data.ip_address')
+INGRESS_PORT=$(echo ${json_info} | jq -r '.data.ingress_port')
 
 echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
-echo "INGRESS_IP = $INGRESS_IP"
 
 if $GB_DEBUG; then
     echo "Here is the output from /addons/self/info"
     curl -s -H "${auth_header}" 'http://hassio/addons/self/info'
     echo -e "\nEnd of output from /addons/self/info"
+    echo "INGRESS_IP = $INGRESS_IP"
+    echo "INGRESS_PORT = $INGRESS_PORT"
 fi
 
 export GB_DEBUG
 export GB_VERSION
 
 cd googlebackup
-python manage.py runserver --noreload $INGRESS_IP:8099
+python manage.py runserver --noreload $INGRESS_IP:$INGRESS_PORT
