57c9a56a44abd7d4604f19ce916d4ca9b795a4ab
Scott McCauley <scottmccauley64@gmail.com>
2019-08-17 09:37:01 -0400

    fixed retrieval of ingress ip address in run.sh

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -6,13 +6,9 @@ OPTIONS_PATH=/data/options.json
 GB_DEBUG="$(jq --raw-output '.debug' $OPTIONS_PATH)"
 GB_VERSION="1.7.0"
 
-echo "HASSIO_TOKEN = $HASSIO_TOKEN"
 auth_header="X-HASSIO-KEY: ${HASSIO_TOKEN}"
-echo "auth_header = $auth_header"
 
-curl -v -H "${auth_header}" 'http://hassio/addons/self/info'
-
-INGRESS_IP="$(curl -s -H \"${auth_header}\" 'http://hassio/addons/self/info' | jq -r '.ip_address')"
+INGRESS_IP="$(curl -s -H \"${auth_header}\" 'http://hassio/addons/self/info' | jq -r '.data.ip_address')"
 
 echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
@@ -21,7 +17,7 @@ echo "INGRESS_IP = $INGRESS_IP"
 if $GB_DEBUG; then
     echo "Here is the output from /addons/self/info"
     curl -s -H "${auth_header}" 'http://hassio/addons/self/info'
-    echo "End of output from /addons/self/info"
+    echo -e "\nEnd of output from /addons/self/info"
 fi
 
 export GB_DEBUG
