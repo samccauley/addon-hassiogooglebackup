0fe390578425f19fc6c2d8c05e99b48f0a70f32e
Scott McCauley <scottmccauley64@gmail.com>
2019-08-22 12:17:28 -0400

    making trailing slash optional in URLs

--- hassiogooglebackup/googlebackup/googlebackup/urls.py
+++ hassiogooglebackup/googlebackup/googlebackup/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('gb/', include('gb.urls')),
-    path('admin/', admin.site.urls),
+    re_path('^gb/?$', include('gb.urls')),
+    re_path('^admin/?$', admin.site.urls),
 ]
