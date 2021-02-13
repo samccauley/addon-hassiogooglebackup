8f8b10648f3fbd944a6c52aba5583f4f082e33cb
Scott McCauley <scottmccauley64@gmail.com>
2021-02-13 13:05:08 -0500

    some CI clean up

--- hassiogooglebackup/run.sh
+++ hassiogooglebackup/run.sh
@@ -7,8 +7,8 @@ GB_DEBUG="$(jq --raw-output '.debug' $OPTIONS_PATH)"
 auth_header="X-HASSIO-KEY: ${HASSIO_TOKEN}"
 json_info=$(curl -s -H "${auth_header}" 'http://hassio/addons/self/info')
 
-GB_SLUG=$(echo ${json_info} | jq -r '.data.slug')
-GB_VERSION=$(echo ${json_info} | jq -r '.data.version')
+GB_SLUG="$(echo "${json_info}" | jq -r '.data.slug')"
+GB_VERSION="$(echo "${json_info}" | jq -r '.data.version')"
 
 echo "GB_DEBUG = $GB_DEBUG"
 echo "GB_VERSION = $GB_VERSION"
@@ -16,7 +16,7 @@ echo "GB_VERSION = $GB_VERSION"
 if $GB_DEBUG; then
     echo "Here is the output from /addons/self/info"
     curl -s -H "${auth_header}" 'http://hassio/addons/self/info'
-    echo -e "\nEnd of output from /addons/self/info"
+    printf '\n%s' "End of output from /addons/self/info"
     echo "GB_SLUG = $GB_SLUG"
 fi
 
@@ -24,5 +24,5 @@ export GB_DEBUG
 export GB_VERSION
 export GB_SLUG
 
-cd googlebackup
+cd googlebackup || exit
 gunicorn -b 0.0.0.0:8000 googlebackup.wsgi:application -k gevent
